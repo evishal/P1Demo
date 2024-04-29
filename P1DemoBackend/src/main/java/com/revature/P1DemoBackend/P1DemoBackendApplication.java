cc537ed1c081a18b2cbc1747812f6afe4ee24ed5
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-04-29 10:32:32 -0400

    started P1Backend

---
+++ P1DemoBackend/src/main/java/com/revature/P1DemoBackend/P1DemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.P1DemoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1DemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1DemoBackendApplication.class, args);
+	}
+
+}
