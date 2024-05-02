be484624698188eba55ca9752e231e5cc1252b43
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-05-02 10:27:47 -0400

    setup for global FE storage

---
+++ p1-demo-frontend/src/globalData/store.ts
@@ -0,0 +1,35 @@
+import { PokemonInterface } from "../interfaces/PokemonInterface";
+import { UserInterface } from "../interfaces/UserInterface";
+
+/* This is a rudimentary implementation of a store, which is basically global data storage
+any data that you want to store globally (visible to the entire app) can reside here
+look into the context APi to see a more industry standard way of doing this (we'll talk later) */
+export const state:any = {
+
+    //we typically want to store user session info on the front end
+    //for personalization as well as role-based security control
+    userSessionData: {
+        userId:0,
+        username:"",
+        //role:"" <- This would be used to determine if a user can do certain things
+    } as UserInterface,
+
+    //storing the last caught pokemon for global display
+    lastCaughtPokemon: {
+        name:"",
+        image:""
+    } as PokemonInterface,
+
+    //Think about your requirements when storing state globally
+    //you only NEED to globally store data you intend to use in multiple components
+    //but you could optimize you code by using global storage to reduce calls to your API
+
+    //we could also store things like base URLs (which I won't use)
+    baseUrl:"http://localhost:8080",
+    basePokemonUrl:"http://localhost:8080/pokemon"
+
+    //TODO: store our incoming JWT 
+
+}
+
+//Side note: typically all global user data will get populated upon successful login
